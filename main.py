5accebbf5d29e0fc1c708d4b5b7baa0f139078ea
kyjiya <kyjiya@github.io>
2022-05-27 00:29:03 +0900

    ADD flare event term main

--- main.py
+++ main.py
@@ -1,63 +1,43 @@
-import requests
 import pprint
+from tqdm import tqdm
 
-from db.engine import dbEngine
-from db.insert import hmi_insert, probability_long_insert
-import db.model as model
-
-
-params = {
-    "ds": "",
-    "op": "rs_list",
-    "key": "",
-}
-
-
-def get_harpnum(url, harpnum, key):
-    params["ds"] = f"hmi.mharp_720s_nrt[{harpnum}][$]"
-    params["key"] = key
-
-    response = requests.get(
-        url,
-        params=params,
-    )
-
-    data = response.json()
-
-    return data
-
-
-def get_sharp_value(url, harpnum, key):
-    params["ds"] = f"hmi.sharp_720s_nrt[{harpnum}][$]"
-    params["key"] = key
-
-    response = requests.get(
-        url,
-        params=params,
-    )
-
-    data = response.json()
-
-    return data
+from db import insert, engine
+from core import get_value
 
 
 if __name__=="__main__":
-    engine = dbEngine()
+    engine = engine.dbEngine()
     
     url = 'http://jsoc.stanford.edu/cgi-bin/ajax/jsoc_info'
 
     harpnum = ""
     key = "HARPNUM"
 
-    data = get_harpnum(url, harpnum, key)
+    harpnum = get_value.get_harpnum(harpnum, key)
+    harpnum = harpnum["keywords"][0]["values"]
 
     key = "T_REC,HARPNUM,NOAA_ARS,TOTUSJH,TOTUSJZ,TOTPOT,USFLUX,ABSNJZH,SAVNCPP"
-    harpnum = data["keywords"][0]["values"][0]
-    
-    data = get_sharp_value(url, harpnum, key)
-    
-    hmi_insert(engine, data)
 
-    probability_long_insert(engine, data)
-    
-    pprint.pprint(data)
+    for k_1 in tqdm(harpnum):
+        data = get_value.get_sharp_value(k_1, key)
+        
+        if "keywords" in data:
+            insert.hmi_insert(engine, data)
+            insert.probability_long_insert(engine, data)
+
+            noaa_ars = data["keywords"][2]["values"][0].split(",")
+
+            short_term_data = []
+            mid_term_data = []
+            for k_2 in tqdm(noaa_ars, leave=False):
+                short_term_data.extend(get_value.get_flare_event(k_2, 'short_term'))
+                mid_term_data.extend(get_value.get_flare_event(k_2, 'middle_term'))
+            
+            # 중복 제거
+            short_term_data = list(set(short_term_data))
+            mid_term_data = list(set(mid_term_data))
+            short_term_data.sort()
+            mid_term_data.sort()
+            insert.probability_insert(engine, data, short_term_data, mid_term_data)
+        else:
+            pass
