bcccfe28160991c3f8e4f46e1386e93a1274eb4b
kyjiya <kyjiya@github.io>
2022-05-27 00:27:38 +0900

    FIX value name

--- db/model.py
+++ db/model.py
@@ -19,7 +19,7 @@ class hmi_sharp(Base):
     )
     date = Column(String(100))
     harpnum = Column(String(100))
-    noaa_ar = Column(String(100))
+    noaa_ars = Column(String(100))
     h_ctotal = Column(Float)    
     j_ztotal = Column(Float)
     rho_tot = Column(Float)
@@ -33,7 +33,7 @@ class hmi_sharp(Base):
             id={self.id!r}, 
             date={self.date!r},
             harpnum={self.harpnum!r},
-            noaa_ar={self.noaa_ar!r},
+            noaa_ars={self.noaa_ars!r},
             h_ctotal={self.h_ctotal!r},
             j_ztotal={self.j_ztotal!r},
             phi={self.phi!r},
@@ -55,7 +55,7 @@ class probability_model_long(Base):
     )
     date = Column(String(100))
     harpnum = Column(String(100))
-    noaa_ar = Column(String(100))
+    noaa_ars = Column(String(100))
     p_l_totusjh = Column(Float)
     p_l_totusjz = Column(Float)
     p_l_totpot = Column(Float)
@@ -69,7 +69,7 @@ class probability_model_long(Base):
             id={self.id!r}, 
             date={self.date!r}, 
             harpnum={self.harpnum!r},
-            noaa_ar={self.noaa_ar!r},
+            noaa_ars={self.noaa_ars!r},
             p_l_totusjh={self.p_l_totusjh!r},
             p_l_totusjz={self.p_l_totusjz!r},
             p_l_totpot={self.p_l_totpot!r},
@@ -92,15 +92,15 @@ class probability_model(Base):
     )
     date = Column(String(100))
     harpnum = Column(String(100))
-    noaa_ar = Column(String(100))
+    noaa_ars = Column(String(100))
     p_totusjh = Column(Float)
     p_totusjz = Column(Float)
     p_totpot = Column(Float)
     p_usflux = Column(Float)
     p_absnjzh = Column(Float)
     p_savncpp = Column(Float)
-    r_m = Column(Float)
     r_s = Column(Float)
+    r_m = Column(Float)
     created_at = Column(DateTime(timezone=True), server_default=func.now())
 
     def __repr__(self):
@@ -108,13 +108,15 @@ class probability_model(Base):
             id={self.id!r}, 
             date={self.date!r},
             harpnum={self.harpnum!r},
-            noaa_ar={self.noaa_ar!r},
+            noaa_ars={self.noaa_ars!r},
             p_totusjh={self.p_totusjh!r},
             p_totusjz={self.p_totusjz!r},
             p_totpot={self.p_totpot!r},
             p_usflux={self.p_usflux!r},
             p_absnjzh={self.p_absnjzh!r},
             p_savncpp={self.p_savncpp!r},
+            r_s={self.r_s!r},
+            r_m={self.r_m!r},
             created_at={self.created_at!r}
         )'''
         
